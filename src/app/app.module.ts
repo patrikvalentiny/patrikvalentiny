d7e8382eae8da80efbe2e4896db86abf7c5b6353
patrikvalentiny <patrikvalentiny@gmail.com>
2023-12-21 18:46:44 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialLinkComponent } from './social-link/social-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialLinkComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/social-link/social-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-link',
+  templateUrl: './social-link.component.html',
+  styleUrl: './social-link.component.css'
+})
+export class SocialLinkComponent {
+
+}
