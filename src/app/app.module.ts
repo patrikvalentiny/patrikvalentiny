0d714e12d4ae3b69d3c614604128d26f37a79622
patrikvalentiny <patrikvalentiny@gmail.com>
2024-01-02 13:54:01 +0100

    add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewComponent
+    OverviewComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
