7b10320c5feaeae410308a4b76e9f45046f862c4
patrikvalentiny <patrikvalentiny@gmail.com>
2024-01-03 21:20:55 +0100

    Revert "try gh pages again"
    This reverts commit 83eff06ef0ef38b9ffcb704962cd79aa251ad39c.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,14 +7,14 @@ import {FormControl} from "@angular/forms";
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent{
-  // private readonly storage = window.localStorage;
-  // isChecked = new FormControl(false);
-  //
-  // constructor() {
-  //   this.isChecked.setValue(this.storage.getItem('darkMode') === 'true');
-  // }
-  // checkBoxClicked() {
-  //   this.storage.setItem('darkMode', this.isChecked.value ? 'true' : 'false');
-  // }
+  private readonly storage = window.localStorage;
+  isChecked = new FormControl(false);
+
+  constructor() {
+    this.isChecked.setValue(this.storage.getItem('darkMode') === 'true');
+  }
+  checkBoxClicked() {
+    this.storage.setItem('darkMode', this.isChecked.value ? 'true' : 'false');
+  }
 
 }
